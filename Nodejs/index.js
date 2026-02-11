b1cedac25604ca7ff81b588ccb6167bdc7df0c96
XYZthedurgeshtiwari <tdurgesh448@gmail.com>
2026-02-11 09:58:00 +0530

    os memory

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -58,3 +58,6 @@ const os=require('os');
 console.log("platform",os.platform());
 console.log("userinfo",os.userInfo());
 console.log("CPU",os.arch());
+
+console.log("free memory",os.freemem());
+console.log("total memory",os.totalmem());
